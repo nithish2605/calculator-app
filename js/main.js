f1fc2d246d601e42c6f2cd552f4959b7336cb89d
NithishKumar <nithishvj2605@gmail.com>
2025-09-14 12:10:50 +0530

    completed the calculator app

--- js/main.js
+++ js/main.js
@@ -1,31 +1,81 @@
 //getting the values
-var keys = document.querySelectorAll(".keys");
-var display =  document.querySelector("#display");
+var keys = document.querySelectorAll(".numkeys");
+var display = document.querySelector("#display");
 let clear = document.querySelector("#refresh");
+var operators = document.querySelectorAll("#operators");
+var equal = document.querySelector("#equal");
+var FirstNum = "";
+var SecondNum = "";
+let operator = null;
 
 //displaying values while clicking the keys
-keys.forEach(key =>{
-    key.addEventListener('click',()=>{
-        let value = key.textContent;
-        let curvalue = display.textContent;
-        if(value === "+" &&curvalue.endsWith('+')){
+keys.forEach(key => {
+    key.addEventListener('click', () => {
+        if (!operator) {
+            FirstNum += key.textContent;
+            // console.log("first number" + FirstNum);
+            display.textContent += key.textContent;
+        }
+        else {
+            SecondNum += key.textContent;
+            // console.log("second num" + SecondNum);
+            display.textContent += key.textContent;
+        }
+    });
+});
+
+//handling operators click
+operators.forEach(op => {
+    op.addEventListener('click', () => {
+        if (FirstNum === "") return;
+        if ((display.textContent).endsWith('+') || (display.textContent).endsWith('-') || (display.textContent).endsWith('*') || (display.textContent).endsWith('/')) {
             return;
         }
-         if(value === "-" &&curvalue.endsWith('-') ){
-            return;
+        if (operator && SecondNum !== "") {
+            calculate();
         }
-         if(value === "X" &&curvalue.endsWith('X')){
-            return;
-        }
-        display.textContent += value ;
-    })
+        operator = op.textContent;
+        display.textContent += operator;
+    });
+});
+
+//handling eqaul clicks
+equal.addEventListener('click', () => {
+    if (FirstNum === "" || SecondNum === "" || !operator) return;
+    calculate();
 });
 
 //clearing the calci
-clear.addEventListener("click",()=>{
-    display.textContent="";
+clear.addEventListener("click", () => {
+    display.textContent = "";
+    FirstNum = "";
+    SecondNum = "";
+    operator = "";
 });
 
-
-
+//function to do operations
+function calculate() {
+    //convert to numbers
+    const num1 = parseFloat(FirstNum);
+    const num2 = parseFloat(SecondNum);
+    let result;
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num2 != 0 ? num1 / num2 : 'error';
+            break;
+    }
+    FirstNum = result.toString();
+    SecondNum = "";
+    operator = null;
+    display.textContent = result;
+}
 
